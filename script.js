72a1333ae28491086c8a44498b7eb5a0d4ee98a4
Brian McCavour <brian.mccavour@sap.com>
2017-05-23 14:09:59 -0400

    siiiiix

--- script.js
+++ script.js
@@ -7,29 +7,6 @@ angular.module('mainApp', [])
 
     controller: function($window, $element, $document) {
 
-        function generateRectangleCss(viewWidth, viewHeight) {
-            var recSize = ((Math.random() * 100) + 100).toFixed();
-            var color = '#' + Math.round(0xffffff * Math.random()).toString(16);
-            var posx = (Math.random() * (viewWidth + 200 - recSize)).toFixed() - 100;
-            var posy = (Math.random() * (viewHeight + 200 - recSize)).toFixed() - 100;
-
-            posy += 150;
-
-            return {
-                'width': recSize + 'px',
-                'height': recSize + 'px',
-                'background-color': color,
-                'position': 'absolute',
-                'left': posx + 'px',
-                'top': posy + 'px',
-                'border': 'solid'
-            };
-        }
-
-        this.generateRandomCss = function() {
-            this.anchorCss = generateRectangleCss($window.innerWidth, $window.innerHeight);
-        }.bind(this);
-
         this.updateAnchorLocation = function() {
             this.anchor.css(this.anchorCss);
         };
@@ -44,7 +21,6 @@ angular.module('mainApp', [])
         }.bind(this);
 
         this.onResize = function() {
-            console.info('vsaef');
             this.windowWidth = $window.innerWidth;
             this.windowHeight = $window.innerHeight;
         }.bind(this);
@@ -63,11 +39,17 @@ angular.module('mainApp', [])
                 'border': 'solid'
             };
 
+            // this.data = 'hi!';
+
             this.popupConfig = { 
-                template: '<div>Hello!</div>',
+                scope: this,
                 trigger: 'click',
                 valign: 'bottom',
-                halign: 'right'
+                halign: 'right',
+                template: '<div style="margin: 10px">' +
+                    '<div>aerfgerfg eragwerg eagr eag rqg erg qgr </div>' + 
+                    '<div>aerfgerfg eragwerg eagr eag rqg erg qgr </div>' +
+                    '{{ $ctrl.data }}<div>',
             };
 
             this.anchor = $('#anchor');
@@ -82,17 +64,55 @@ angular.module('mainApp', [])
 	templateUrl: "/appTemplate.html"
 })
 
-.directive('yPosition', function() {
+.directive('yPosition', function($compile) {
   return {
     restrict: 'A',
+    scope: false,
     scope: {
         config: '=yPosition'
     },
     controllerAs: '$ctrl',
-    controller: function($scope, $element) {
-
+    controller: function($scope, $element, $document, $window, $attrs, $interpolate) {
 
         function adjustToView(coords) {
+
+            // HORIZONTAL POSITION / SIZE
+            // if width of popup is wider then viewport, position is full width
+            if (coords.width >= $window.innerWidth) {
+                coords.left = 0;
+                coords.width = $window.innerWidth;
+            } else {
+                // if right edge of popup would be off the screen to the right, then
+                // move it left until right edge of popup is on right side of viewport
+                if (coords.left + coords.width >= $window.innerWidth) {
+                    coords.left = $window.innerWidth - coords.width;
+                }
+                // if left edge is off the screen to left, move to 0
+                if (coords.left <= 0) {
+                    coords.left = 0;
+                }
+                // resize width if necessary to fit in viewport
+                coords.width = Math.min(coords.width, $window.innerWidth - coords.left);
+            }
+
+            // VERTICAL POSITION / SIZE
+            // if height of popup is taller than the viewport, position is full height
+            if (coords.height >= $window.innerHeight) {
+                coords.top = 0;
+                coords.height = $window.innerHeight;
+            } else {
+                // if bottom edge of popup would be off the screen to the bottom, then
+                // move it up until bottom edge of popup is on bottom side of viewport
+                if (coords.top + coords.height >= $window.innerHeight) {
+                    coords.top = $window.innerHeight - coords.height;
+                }
+                // if top edge is off the screen to top, move down to 0
+                if (coords.top <= 0) {
+                    coords.top = 0;
+                }
+                // resize width if necessary to fit in viewport
+                coords.height = Math.min(coords.height, $window.innerHeight - coords.top);
+            }
 
         }
 
@@ -124,16 +144,20 @@ angular.module('mainApp', [])
         }
 
         this.recomputeLocation = function() {
-            var anchorBounds = $element[0].getBoundingClientRect();
-            var popupBounds = this.popupElement[0].getBoundingClientRect();
+            console.info('fwefewf');
+            if (this.popupElement) {
+                var anchorBounds = $element[0].getBoundingClientRect();
+                var popupBounds = this.popupElement[0].getBoundingClientRect();
 
-            var position = calculatePreferredPosition(anchorBounds, 
-                popupBounds.width, popupBounds.height, this.config.valign, this.config.halign);
-            this.popupElement.css(position);
+                var position = calculatePreferredPosition(anchorBounds, 
+                    popupBounds.width, popupBounds.height, this.config.valign, this.config.halign);
+                adjustToView(position);
+                this.popupElement.css(position);
+            }
         }.bind(this);
 
         this._validateConfig = function() {
-            if (!this.config.template) {
+            if (!this.config.template && !this.config.templateUrl) {
                 throw "yPosition config missing template!";
             }
         }.bind(this);
@@ -167,23 +191,28 @@ angular.module('mainApp', [])
 
         this.show = function() {
             if (!this.popupDisplayed) {
-                var PRE = "<div style='position: fixed; background-color: #DCDCDC' data-uuid=" + this.uuid + ">";
+                var PRE = "<div style='position: absolute; background-color: #DCDCDC' data-uuid=" + this.uuid + ">";
                 var POST = "</div>";
                 this.popupElement = angular.element(PRE + this.config.template + POST);
+                var scope = this.config.scope.$scope || $scope;
+                $compile(this.popupElement)(scope);    
+                
                 this.popupElement.appendTo(this.bodyElement).ready(this.recomputeLocation);
             }
             this.popupDisplayed = true;
         };
 
         this.$doCheck = function() {
-            if (!this.oldConfig) {
-                this.oldConfig = angular.copy(this.config);
-                this.recomputeLocation();
-            }
-            if ((this.config.halign !== this.oldConfig.halign) ||
-                (this.config.valign !== this.oldConfig.valign)) {
-                this.oldConfig = angular.copy(this.config);
-                this.recomputeLocation();
+            if (this.config) {
+                if (!this.oldConfig) {
+                    this.oldConfig = angular.copy(this.config);
+                    this.recomputeLocation();
+                }
+                if ((this.config.halign !== this.oldConfig.halign) ||
+                    (this.config.valign !== this.oldConfig.valign)) {
+                    this.oldConfig = angular.copy(this.config);
+                    this.recomputeLocation();
+                }
             }
         }.bind(this);
 
@@ -193,10 +222,13 @@ angular.module('mainApp', [])
             this.popupDisplayed = false;
             this.bodyElement = $('body');
             this.config = $scope.config;
-            
+
+            // var x = $interpolate($attrs.yPosition)();
+            // this.config = $scope.$eval($attrs.yPosition);
             this._validateConfig();
             this._setupTigger();
 
+            angular.element($document[0].body).on('scroll', this.recomputeLocation);
 
         };
 
